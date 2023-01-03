6efa23952a89ee505656424c45fca2c0287aa5a4
[R*] Dante <102482809+unknownkillid@users.noreply.github.com>
2023-01-03 23:28:43 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 let startGame = document.getElementById('start-game-container');
 let settingsMenu = document.getElementById('sett-menu');
 let shopMenu = document.getElementById('shop-container');
+let rocket = document.getElementById("rocket");
 let clickAudio = new Audio();
 let startAudio = new Audio();
+let clickMuted = new Audio();
+let gameMusic = new Audio();
+
+gameMusic.src = "Items/sounds/music/gameMusic.mp3";
 clickAudio.src = "Items/sounds/clicksound.mp3";
 startAudio.src = "Items/sounds/startGame.wav";
 
-function startGameSound(){
+function startGameSound() {
   startAudio.play();
 }
 
-function onClickSound(){
+function muteAudio() {
+  clickAudio.pause();
+}
+
+function onClickSound() {
   clickAudio.play();
 }
 
-function pauseAllAudio(){
+function pauseAllAudio() {
   clickAudio.pause();
 }
 
@@ -64,9 +73,9 @@ function startGameOtherHide() {
 }
 
 function closeShopMenu() {
-    if (shopMenu.style.display === 'block') {
-      shopMenu.style.display = 'none';
-    } 
+  if (shopMenu.style.display === 'block') {
+    shopMenu.style.display = 'none';
+  }
 }
 
 function xSettingsMenu() {
@@ -86,18 +95,16 @@ let y = 500;
 const left_side = 0;
 const right_side = 580;
 const top_max = 100;
-const bottom_max = 560;
+const bottom_max = 490;
 let angle = 0;
- 
 var currentBulletId = 0;
+
 Start.addEventListener("click", () => {
   accelerateRocket();
 });
 function accelerateRocket() {
-  // Get the current position of the rocket
-  const rocket = document.getElementById("rocket");
+  
   document.addEventListener("keydown", (event) => {
-    // Attach keys to movement
 
     switch (event.key) {
       case "ArrowUp":
@@ -114,7 +121,7 @@ function accelerateRocket() {
         if (x - 10 < left_side) {
           break;
         } else {
-          rocket.style.transform = 'rotateZ(-84deg)';
+          rocket.style.transform = 'rotateZ(-84deg)'
         }
 
         x -= 50;
@@ -144,29 +151,32 @@ function accelerateRocket() {
 
     rocket.style.left = x + "px";
     rocket.style.top = y + "px";
+
+
     if (event.code === "Space") {
       currentBulletId++;
       const tyvia = document.getElementById("bullet-animated");
-      tyvia.id = `bullet${currentBulletId}`;
+      tyvia.id = `bullet-animated${currentBulletId}`;
       document.body.appendChild(tyvia);
-      const bullet = document.getElementById(`bullet${currentBulletId}`);
+      const bullet = document.getElementById(`bullet-animated${currentBulletId}`);
       bullet.style.position = "absolute";
-      bullet.style.top = y - 60 + "px";
-      bullet.style.left = x + 40 + "px";
+      bullet.style.top = y - -130 + "px";
+      bullet.style.left = x + rocket + "px";
       bullet.style.height = "50px";
       bullet.style.width = "50px";
 
-      if (event.code = "space") {
-        tyvia.style.display = 'block';
+      if (event.code === "Space") {
+        bullet.style.display = 'block';
+       
       }
-
+      
 
       let position = bullet.offsetTop;
       const interval = setInterval(function () {
         position -= 20;
         bullet.style.top = position + "px";
 
-        if (position <= MIN_Y - 40) {
+        if (position <= bottom_max - 200) {
           bullet.remove();
           clearInterval(interval);
         }
@@ -174,4 +184,42 @@ function accelerateRocket() {
     }
   });
 }
+
+// important code
+
+// let rame = document.querySelector('#somebutton');
+// rame.addEventListener('click', function(){
+//   if (rame.style.color = 'red'){
+//     startGame.style.display = 'none';
+//     settingsMenu.style.display = 'block';
+//   }
+// })
+
+// გავაკეთოთ რაკეტის გასწორების ფუნქცია querryselector-ით
+
+let mute = document.querySelector('#check-sound');
+
+mute.addEventListener('click', function () {
+  if (mute.checked) {
+    clickAudio.volume = 0;
+    startAudio.volume = 0;
+  } else {
+    clickAudio.volume = 1;
+    startAudio.volume = 1;
+  }
+})
+
+let audioElement = document.getElementById('my-audio');
+let playButton = document.getElementById('check-sett');
+
+audioElement.play();
+
+playButton.addEventListener('click', function() {
+  if (playButton.checked) {
+    audioElement.pause();
+  } else {
+    audioElement.play();
+  }
+});
+
 
